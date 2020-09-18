cf77fa833aa615f2ce7f8d0c887ca20374136b60
Daniel Porta <daniel.porta@dfki.de>
2020-09-18 10:12:05 +0200

    refinements

---
+++ modules/de.dfki.cos.basys.aas.component/src/main/java/de/dfki/cos/basys/aas/component/AasComponent.java
@@ -0,0 +1,8 @@
+package de.dfki.cos.basys.aas.component;
+
+import org.eclipse.basyx.aas.metamodel.map.descriptor.AASDescriptor;
+import org.eclipse.basyx.submodel.metamodel.map.identifier.Identifier;
+
+public interface AasComponent extends ModelProviderComponent<AASDescriptor> {
+
+}
---
+++ modules/de.dfki.cos.basys.aas.component/src/main/java/de/dfki/cos/basys/aas/component/SubmodelComponent.java
@@ -0,0 +1,9 @@
+package de.dfki.cos.basys.aas.component;
+
+import org.eclipse.basyx.aas.metamodel.map.descriptor.SubmodelDescriptor;
+import org.eclipse.basyx.submodel.metamodel.map.identifier.Identifier;
+
+public interface SubmodelComponent extends ModelProviderComponent<SubmodelDescriptor> {
+
+	Identifier getSubmodelId();
+}
